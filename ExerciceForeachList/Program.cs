c9ec426ee60eaa2078e40f8df16a628240101a7e
unknown <gabrieljimenez@paucasesnovescifp.cat>
2020-10-21 18:44:23 +0200

    Primera parte del ejercicio1, crea una lista de string y la muestra por pantalla

---
+++ ExerciceForeachList/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciceForeachList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nom = AñadirPersona();
+
+            foreach(string hola in nom)
+            {
+                Console.WriteLine($"Hola  { hola }");
+            }
+            Console.ReadLine();
+        }
+
+        static List<string> AñadirPersona()
+        {
+            List<string> nom = new List<string>();
+
+            nom.Add("Jhon");
+            nom.Add("Mary");
+            nom.Add("Sue");
+            nom.Add("Greg");
+            nom.Add("Yolanda");
+            nom.Add("Jose");
+            nom.Add("Bill");
+            
+            return nom;
+        }
+
+        
+    }
+}
